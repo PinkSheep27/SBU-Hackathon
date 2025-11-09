efaba0d319f7140729d9053a79f3f4cf7c8dd479
Rainyy21 <simonzheng950@gmail.com>
2025-11-08 20:39:17 -0500

    pasted all my ai file

---
+++ app/api/chat/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { chatToGemini } from "@/app/utils/geminiHelper";
+import { ChatHistory, ChatSetting } from "@/app/exportType/types";
+
+
+export async function POST(request: Request) {
+  try {
+    const { userMessage, history , settings } = await request.json() as {
+      userMessage :string;
+      history : ChatHistory;
+      settings : ChatSetting;
+    };
+
+    const aiResponse = await chatToGemini(userMessage , history, settings);
+    return NextResponse.json({response : aiResponse}); 
+  }
+  catch(error){
+    console.error(error);
+    return NextResponse.json(
+      {error: "Error with the model response "},
+      {status : 500}
+    );
+  }
+}
